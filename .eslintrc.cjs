e2d5df7ef15dc508bbd9abdbde69412ebbcd2a4f
lyj.lee <lyj.lee@apptest.ai>
2025-10-22 18:11:14 +0900

    add: prettier, eslint 설정 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+/* eslint 설정: TS + Prettier */
+module.exports = {
+  env: { node: true, es2022: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    'import/resolver': { typescript: true },
+  },
+  rules: {
+    'prettier/prettier': ['error'],
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling', 'index']],
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+  },
+  ignorePatterns: ['dist', 'node_modules', '*.config.js', '*.config.cjs'],
+}
