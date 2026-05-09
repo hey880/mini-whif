ded439602e38cbb6c116d85579f6918c9b335fce
lyj.lee <lyj.lee@apptest.ai>
2026-05-10 02:48:18 +0900

    delete: legacy 파일 삭제

--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from 'fastify'
-import authRoute from './auth'
-
-const routes = async (fastify: FastifyInstance) => {
-  await fastify.register(authRoute, { prefix: '/auth' })
-}
-
-export default routes
